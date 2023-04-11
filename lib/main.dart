33c5c26285398787940b1e5a377967d329bac08e
Omar Mohamed <75180396+omardavinci10@users.noreply.github.com>
2023-04-11 22:24:28 +0200

    Added required functionality with bonus random images

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,106 +12,133 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Color? generatedColor;
+  bool colors = true;
+  String selectedRandomImg = '';
+  List<String> randomBackgroundImages = [
+    'https://docs.flutter.dev/assets/images/dash/early-dash-sketches2.jpg',
+    'https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067__340.png',
+    'https://docs.flutter.dev/assets/images/dash/early-dash-sketches3.jpg',
+    'https://docs.flutter.dev/assets/images/dash/early-dash-sketches5.jpg',
+    'https://docs.flutter.dev/assets/images/dash/dash-prototypes2.jpg',
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  randomColor() {
+    // Colors can be accessed by Color ( int value )
+    // So I'll generate this int value from 0 to 0xFF FF FF FF
+    // every 2 bytes indicates something
+    // FF FF FF FF (alpha, red, green, blue)
+
+    int nextValue = (Random().nextDouble() * 0xFFFFFFFF).toInt();
+
+    // Here I'm just making sure that this is a new value
+    // While it's still the same value I'll keep generating new values
+    while (generatedColor?.value == nextValue) {
+      nextValue = (Random().nextDouble() * 0xFFFFFFFF).toInt();
+    }
+
+    // When I finally have new value, set my background to it
+    generatedColor = Color(nextValue);
+    debugPrint('new background color');
+  }
+
+  randomImage() {
+    int nextValue = Random().nextInt(randomBackgroundImages.length);
+
+    while (selectedRandomImg == randomBackgroundImages[nextValue]) {
+      nextValue = Random().nextInt(randomBackgroundImages.length);
+    }
+
+    // When I finally have new value, set my background to it
+    selectedRandomImg = randomBackgroundImages[nextValue];
+    debugPrint('new background image');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: InkWell(
+        onTap: () {
+          colors ? randomColor() : randomImage();
+          setState(() {});
+        },
+        child: Container(
+          height: screenHeight,
+          width: screenWidth,
+          color: colors ? generatedColor : null,
+          decoration: colors
+              ? null
+              : selectedRandomImg.isNotEmpty
+                  ? BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(selectedRandomImg),
+                        fit: BoxFit.fill,
+                      ),
+                    )
+                  : null,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FittedBox(
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      'Hey there',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color:
+                            Color((Random().nextDouble() * 0xFFFFFFFF).toInt())
+                                .withOpacity(1.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          colors = !colors;
+          debugPrint(
+              'toggled! ${colors ? 'changing colors' : 'changing images'} now');
+          colors ? randomColor() : randomImage();
+          setState(() {});
+        },
+        tooltip: 'Toggle',
+        backgroundColor: generatedColor,
+        elevation: 10,
+        child: Icon(
+          Icons.ac_unit,
+          color: Color((Random().nextDouble() * 0xFFFFFFFF).toInt())
+              .withOpacity(1.0),
+        ),
+      ),
     );
   }
 }
